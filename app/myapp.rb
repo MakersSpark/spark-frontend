ed5f115541a0e15cb80759c845b557e9f57f72e9
byverdu <byverdu@gmail.com>
2014-10-13 23:30:51 +0100

    added forecast_io API, returns forecast after submiting a button

--- app/myapp.rb
+++ app/myapp.rb
@@ -1,4 +1,8 @@
 require 'sinatra'
+require 'forecast_io'
+require 'JSON'
+require 'open-uri'
+
 require 'omniauth-openid'
 require 'openid'
 require 'openid/store/filesystem'
@@ -6,9 +10,22 @@ require 'gapps_openid'
 
 
 
-
 get '/' do
-	erb :index
+erb :index
+
 end
 
+post '/' do
+	forecast_raw = open("https://api.forecast.io/forecast/967ecda5e55eea73c15e3a4ce315e508/51.5231,-0.0871").read   
 
+	forecast = JSON.parse(forecast_raw)
+
+	if forecast  
+	  @forecast = forecast["minutely"]["summary"]
+	end
+	erb :index
+end
+   
+#
+# print my daily mood, 1 to 5 emoticons, prints name, date and image 
+#
